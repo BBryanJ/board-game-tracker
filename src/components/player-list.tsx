0a020371a4b24fdfcdb6dc193e4a6906338dc622
BBryanJ <boeing.bryan242@gmail.com>
2024-10-05 13:46:07 -0400

    feat(player-list): Add in editable player input with buttons

--- src/components/player-list.tsx
+++ src/components/player-list.tsx
@@ -1,4 +1,8 @@
+import { CircleX, Pencil, Save } from 'lucide-react';
 import { CardContent } from './ui/card';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { useState } from 'react';
 
 interface Player {
 	id: number;
@@ -19,18 +23,51 @@ const colorVariants: Record<string, string> = {
 
 export function PlayerList({ players }: PlayerListProps) {
 	return (
-		<CardContent className='flex flex-col gap-2 justify-center items-center h-full'>
+		<CardContent className='flex h-full flex-col items-center justify-center gap-2'>
 			{players.map(player => {
-				const playerColor = player.color;
-				return (
-					<div key={player.id} className='flex gap-2 items-center'>
-						<div
-							className={`w-8 h-8 rounded-md border-2 border-gray-500 ${colorVariants[playerColor]}`}
-						/>
-						<span>{player.name}</span>
-					</div>
-				);
+				return <Player key={player.id} player={player} />;
 			})}
 		</CardContent>
+	);
+}
+
+function Player({ player }: { player: Player }) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	return (
+		<div className='group flex w-full items-center justify-start gap-2'>
+			<div
+				className={`size-8 flex-shrink-0 rounded-md border-2 border-gray-500 ${colorVariants[player.color]}`}
+			/>
+			{isEditing ? <Input value={player.name} /> : <span>{player.name}</span>}
+			{isEditing && (
+				<Button
+					className='flex-shrink-0'
+					variant='ghost'
+					size='icon'
+					onClick={() => setIsEditing(false)}
+				>
+					<CircleX className='size-4' />
+				</Button>
+			)}
+			<Button
+				className='flex-shrink-0'
+				variant='ghost'
+				size='icon'
+				onClick={() => {
+					if (isEditing) {
+						setIsEditing(false);
+					} else {
+						setIsEditing(true);
+					}
+				}}
+			>
+				{isEditing ? (
+					<Save className='size-4' />
+				) : (
+					<Pencil className='size-4 opacity-0 group-hover:opacity-100' />
+				)}
+			</Button>
+		</div>
 	);
 }
